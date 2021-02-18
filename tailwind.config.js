dc1c87cbd043596d2c49ac64730644da7bc3ca96
dickyp <richard.peckett@outlook.com>
2021-02-18 12:00:45 +0000

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+const { colors } = require('tailwindcss/defaultTheme');
+
+module.exports = {
+  theme: {
+    colors: {
+      primary: colors.blue,
+      secondary: colors.blue,
+      neutral: colors.gray,
+      white: '#FFF',
+    },
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+};
