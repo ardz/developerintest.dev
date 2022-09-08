28035286536ceabea1debaa6fbf7bebfafb5d6b9
Richard Peckett <richard.peckett@outlook.com>
2022-09-08 19:43:08 +0100

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-f44b6aff0d4fcb98ea59.js"
+    "url": "webpack-runtime-adda45837b7b5339cec1.js"
   },
   {
     "url": "framework-3e6dec6139dadaa7d0c7.js"
@@ -40,7 +40,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "9e1e28e5252f9221e5e02c555125cce8"
+    "revision": "b30652f84a007fcda8a6e75406943471"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-16703ee5599528db9f93.js"
@@ -51,7 +51,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "c27c56aacca547f418e37571f24b670e"
+    "revision": "7dc4c811a4941a73e1fadb89fdac16be"
   },
   {
     "url": "polyfill-38cce18603b1d20cac17.js"
